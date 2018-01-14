c3ee77fb55467797392224a9c0c0e0d3b9c0d9dd
Tiara Allidina <tiara.allidina@gmail.com>
2018-01-14 01:35:51 -0800

    Display jobs and account info in Android app

--- EmployMePls/app/src/main/java/com/example/tiara/employmepls/AccountActivity.java
+++ EmployMePls/app/src/main/java/com/example/tiara/employmepls/AccountActivity.java
@@ -3,7 +3,11 @@ package com.example.tiara.employmepls;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * Created by Tiara on 2018-01-13.
@@ -16,12 +20,29 @@ public class AccountActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_account);
-        ((TextView)findViewById(R.id.page_title)).setText("Account");
+        ((TextView)findViewById(R.id.page_title)).setText("My Account");
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null){
+            return;
+        }
+
+        ((TextView)findViewById(R.id.name)).setText(user.getDisplayName());
+        ((TextView)findViewById(R.id.email)).setText(user.getEmail());
+        ((TextView)findViewById(R.id.resume_uploaded)).setText("");
+        ((TextView)findViewById(R.id.jobs_applied_to)).setText("");
+
+        findViewById(R.id.home_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
     }
 
     @Override
     public void onBackPressed() {
-        //super.onBackPressed();
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
         finish();
--- EmployMePls/app/src/main/java/com/example/tiara/employmepls/JobsActivity.java
+++ EmployMePls/app/src/main/java/com/example/tiara/employmepls/JobsActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -12,14 +15,16 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Tiara on 2018-01-13.
  */
 
 public class JobsActivity extends AppCompatActivity {
-    List<>
+    List<Job> jobs = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,17 @@ public class JobsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_jobs);
 
         ((TextView)findViewById(R.id.page_title)).setText("Jobs");
+
+        findViewById(R.id.home_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+        ListView jobsListView = (ListView)findViewById(R.id.jobs_listview);
+        final ArrayAdapter jobsAdapter = new JobsAdapter(this, R.layout.job_row, jobs);
+        jobsListView.setAdapter(jobsAdapter);
 
         // Connect to the Firebase database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -36,14 +52,53 @@ public class JobsActivity extends AppCompatActivity {
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                // dataSnapshot.getChildren()
+                Log.d("Jobs", ""+dataSnapshot.getChildren());
+                jobsAdapter.clear();
                 for (DataSnapshot ds : dataSnapshot.getChildren()){
+                    Map data = ((Map)ds.getValue());
+                    if (data == null){
+                        continue;
+                    }
+                    Job job = new Job();
+                    Object title = data.get("title");
+                    if (title != null) {
+                        job.setTitle((String)title);
+                    }
+                    Object description = data.get("description");
+                    if (description != null) {
+                        job.setDescription((String)description);
+                    }
+                    Object requirements = data.get("requirements");
+                    if (requirements != null) {
+                        job.setRequirements((String)requirements);
+                    }
+                    Object salary = data.get("salary");
+                    if (salary != null) {
+                        job.setSalary((String)salary);
+                    }
+                    Object startDate = data.get("startDate");
+                    if (startDate != null) {
+                        job.setStartDate((String)startDate);
+                    }
+                    Object type = data.get("type");
+                    if (type != null) {
+                        job.setType((String)type);
+                    }
+                    Object company = data.get("company");
+                    if (company != null) {
+                        job.setCompany((String)company);
+                    }
+                    Object location = data.get("location");
+                    if (location != null) {
+                        job.setLocation((String)location);
+                    }
+
+                    jobs.add(job);
+                   // jobsAdapter.clear();
+                    //jobsAdapter.addAll(jobs);
 
                 }
-//                facts = (List<String>) dataSnapshot.getValue();
-//                informationManager.setFacts(facts);
-//                //for ()
-//                Log.d("Firebase Database", facts.size() + " facts");
+                Log.d("Firebase Database", jobs.size() + " jobs");
             }
 
             @Override
